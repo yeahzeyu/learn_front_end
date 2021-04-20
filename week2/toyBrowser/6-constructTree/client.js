61a052045f214d4f0ce636b2d45d464de356f7f1
yeahzeyu <819899147@qq.com>
2021-04-20 13:22:29 +0800

    week2lesson6

---
+++ week2/toyBrowser/6-constructTree/client.js
@@ -0,0 +1,242 @@
+const net = require("net");
+const parser = require("./parser.js")
+
+class Request {
+    constructor(options) {
+        this.method = options.method || "GET";
+        this.host = options.host || '127.0.0.1';
+        this.port = options.port || 80;
+        this.path = options.path || "/";
+        this.body = options.body || {};
+        //现阶段的toy browser只支持application/json或application/x-www-form-urlencoded（默认）两种content type，其他类型无法正常解析
+        this.bodyText = !options.headers ?
+            '' : options.headers["Content-Type"] === "application/json" ?
+                JSON.stringify(this.body) : !options.headers["Content-Type"] || options.headers["Content-Type"] === "application/x-www-form-urlencoded" ?
+                    Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&') : '';
+        this.headers = {
+            ["Content-Type"]: "application/x-www-form-urlencoded",
+            ...(options.headers || {}),
+            ["Content-Length"]: this.bodyText.length
+        };
+    }
+    //发送http请求
+    send(connection) {
+        return new Promise((resolve, reject) => {
+            //send过程中，会逐步收到response，直到最后把response构造好后，再执行resolve，所以有必要设计一个response parser，而不是直接设计一个response类，这样parser可以用逐步接收response的信息，最终构建的response对象的各个不同的部分
+            const parser = new ResponseParser;
+            if (connection) {
+                //toString负责把收集到的信息按照request的格式构造
+                connection.write(this.toString());
+            } else {
+                connection = net.createConnection({
+                    host: this.host,
+                    port: this.port
+                }, () => {
+                    connection.write(this.toString())
+                })
+            }
+            connection.on('data', (data) => {
+                console.log(data.toString());
+                parser.receive(data.toString());
+                if (parser.isFinished) {
+                    resolve(parser.response);
+                    connection.end();
+                }
+            })
+            connection.on('error', (err) => {
+                console.log(err)
+                reject(err);
+                connection.end();
+            });
+        });
+    }
+
+    toString() {
+        return `${this.method} ${this.path} HTTP/1.1\r\n${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\n\r\n${this.bodyText}`;
+    }
+}
+
+class ResponseParser {
+    constructor() {
+        //状态机有很多种不同的写法，这里采用的是常量配合if语句实现，但从性能和代码管理的角度，都不如使用函数的方式
+        this.WAITING_STATUS_LINE = 0; //初始状态，接收到\r的时候，不会立刻切到WAITING_HEADER状态，而是再等待一个line end的符号
+        this.WAITING_STATUS_LINE_END = 1; //以\r\n结束
+        this.WAITING_HEADER_NAME = 2; //header的key
+        this.WAITING_HEADER_SPACE = 3; //header的空格
+        this.WAITING_HEADER_VALUE = 4; //header的value
+        this.WAITING_HEADER_LINE_END = 5; //header的结束
+        this.WAITING_HEADER_BLOCK_END = 6; //header结束后空一行
+        this.WAITING_BODY = 7;//因为body的格式不固定，所以不能在同一个response parser里面解决
+        this.current = this.WAITING_STATUS_LINE;
+        this.statusLine = "";
+        this.headers = {};
+        this.headerName = "";
+        this.headerValue = "";
+        this.bodyParser = null; //跟head相关，不能单独创建
+    }
+    get isFinished() {
+        return this.bodyParser && this.bodyParser.isFinished;
+    }
+    get response() {
+        this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/);
+        return {
+            statusCode: RegExp.$1,
+            statusText: RegExp.$2,
+            headers: this.headers,
+            body: this.bodyParser.content.join('')
+        }
+    }
+    receive(string) {
+        for (let i in string) {
+            //处理字符串，使用到了状态机，charAt能支持中文
+            this.receiveChar(string.charAt(i));
+        }
+    }
+    receiveChar(char) {
+        //有限状态机的代码
+        if (this.current === this.WAITING_STATUS_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_STATUS_LINE_END;
+            } else {
+                this.statusLine += char;
+            }
+        } else if (this.current === this.WAITING_STATUS_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        } else if (this.current === this.WAITING_HEADER_NAME) {
+            if (char === ':') {
+                this.current = this.WAITING_HEADER_SPACE;
+            } else if (char === '\r') {
+                this.current = this.WAITING_HEADER_BLOCK_END;
+                //在head结束时创建body parser
+                //transfer encoding可以有多个值，但默认的是chunked
+                if (this.headers['Transfer-Encoding'] === 'chunked')
+                    this.bodyParser = new TrunkedBodyParser();
+            } else {
+                this.headerName += char;
+            }
+        } else if (this.current === this.WAITING_HEADER_SPACE) {
+            if (char === ' ') {
+                this.current = this.WAITING_HEADER_VALUE;
+            }
+        } else if (this.current === this.WAITING_HEADER_VALUE) {
+            if (char == '\r') {
+                this.current = this.WAITING_HEADER_LINE_END;
+                this.headers[this.headerName] = this.headerValue;
+                this.headerName = "";
+                this.headerValue = "";
+            } else {
+                this.headerValue += char;
+            }
+        } else if (this.current === this.WAITING_HEADER_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        } else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_BODY;
+            }
+        } else if (this.current === this.WAITING_BODY) {
+            //使用专用的body parser处理body部分
+            this.bodyParser.receiveChar(char);
+        }
+    }
+}
+
+class TrunkedBodyParser {
+    //跟response parser非常相似
+    constructor() {
+        //trunk body的结构是一个长度后面跟着一个trunk的内容，遇到一个长度为0的trunk时，整个body就结束了，因为trunk里面可以是任何的字符，所以不能通过特定字符来控制分割
+        this.WAITING_LENGTH = 0;
+        this.WAITING_LENGTH_LINE_END = 1;
+        this.READING_TRUNK = 2; //要想退出READING_TRUNK状态，必须去计算chunk的长度，等待出现长度0的情况，不是一个严格的米利状态机了
+        this.WAITING_NEW_LINE = 3;
+        this.WAITING_NEW_LINE_END = 4;
+        this.length = 0;
+        this.content = [];
+        this.isFinished = false;
+        this.current = this.WAITING_LENGTH;
+    }
+    receiveChar(char) {
+
+        if (this.current === this.WAITING_LENGTH) {
+            if (char === '\r') {
+                if (this.length === 0) {
+                    this.isFinished = true;
+                }
+                this.current = this.WAITING_LENGTH_LINE_END;
+            } else {
+                //length是16进制的，边读取边将其转为10进制，方便下面的循环
+                this.length *= 16;
+                this.length += parseInt(char, 16);
+            }
+        } else if (this.current === this.WAITING_LENGTH_LINE_END) {
+            if (char === '\n') {
+                this.current = this.READING_TRUNK;
+            }
+        } else if (this.current === this.READING_TRUNK) {
+            this.content.push(char);
+            //this.length--; //纯英文、数字的情况
+            this.length = this.length - this.getByte(char); //中文字符占用了多个字节
+            if (this.length === 0)
+                this.current = this.WAITING_NEW_LINE;
+        } else if (this.current === this.WAITING_NEW_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_NEW_LINE_END;
+            }
+        } else if (this.current === this.WAITING_NEW_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_LENGTH;
+            }
+        }
+    }
+    //为了body中能支持出现中文，需解析一个字符占多少字节
+    getByte(char) {
+        if (char.charCodeAt(0) <= 0x007f) return 1;
+        if (char.charCodeAt(0) <= 0x07ff) return 2;
+        if (char.charCodeAt(0) <= 0xffff) return 3;
+        return 4;
+    }
+    /*
+        英文字母和中文汉字在不同字符集编码下的字节数:
+        英文字母：
+            字节数 : 1;编码：GB2312
+            字节数 : 1;编码：GBK
+            字节数 : 1;编码：GB18030
+            字节数 : 1;编码：ISO-8859-1
+            字节数 : 1;编码：UTF-8
+            字节数 : 4;编码：UTF-16
+            字节数 : 2;编码：UTF-16BE
+            字节数 : 2;编码：UTF-16LE
+
+        中文汉字：
+            字节数 : 2;编码：GB2312
+            字节数 : 2;编码：GBK
+            字节数 : 2;编码：GB18030
+            字节数 : 1;编码：ISO-8859-1
+            字节数 : 3;编码：UTF-8
+            字节数 : 4;编码：UTF-16
+            字节数 : 2;编码：UTF-16BE
+            字节数 : 2;编码：UTF-16LE
+    */
+}
+
+void async function () {
+    let request = new Request({
+        method: "POST",
+        host: "127.0.0.1",
+        port: "8088",
+        path: "/",
+        headers: {
+            ["X-Foo2"]: "customed"
+        },
+        body: {
+            name: "WoodyYip"
+        }
+    });
+    //拆分文件
+    let response = await request.send();
+    //如果是真正的浏览器，这里必须是异步分段地返回的，而不是返回整个body给parser
+    let dom = parser.parseHTML(response.body);
+}();
---
+++ week2/toyBrowser/6-constructTree/parser.js
@@ -0,0 +1,409 @@
+//词法分析 -> 语法分析
+//html的的语法用一个栈就能完成分析，toy browser光用一个栈就能完成分析，而真正的浏览器还要家很多特殊的处理实现容错性
+let currentToken = null;
+let currentAttribute = null;
+
+//type属性值会与tag中的type属性重名，导致赋值覆盖，改成其他名字，例如tagType
+let stack = [{ tagType: "document", children: [] }]; //配对良好的HTML最后的栈应该是空的，这样不方便把栈拿出来，所以要设计一个初始的根节点，而HTML的根节点也是document
+
+//最后状态机里的所有状态创建完token之后要把它在同一个出口输出
+function emit(token) {
+    if (token.tagType === "text") //暂时忽略文本节点
+        return;
+    //console.log(token)
+    let top = stack[stack.length - 1]; //栈顶是最后一个元素
+    if (token.tagType == "startTag") {
+        //进行入栈操作
+        let element = {
+            type: "element",
+            children: [],
+            attributes: []
+        };
+        element.tagName = token.tagName;
+        for (let p in token) {
+            if (p != "tagType" && p != "tagName") {
+                element.attributes.push({
+                    mame: p,
+                    value: token[p]
+                })
+            }
+        }
+        top.children.push(element);
+        element.parent = top;
+        if(!token.isSelfClosing) {
+            stack.push(element);
+        }
+        currentTextNode = null;
+    } else if(token.tagType == "endTag") {
+        if(top.tagName != token.tagName) {
+            console.log(top.tagName);
+            console.log(token.tagName);
+            throw new Error("Tag start end doesn't match!");
+        } else {
+            stack.pop();
+        }
+        currentTextNode = null;
+    }
+}
+
+const EOF = Symbol("EOF"); //EOF: End Of File，ES6 引入了一种新的原始数据类型 Symbol ，表示独一无二的值，最大的用法是用来定义对象的唯一属性名，也可以用来定义互不相等的一组常量。
+
+//尝试把状态机里的状态实现一部分，把HTML里所有的tag做一个解析（大致有三种：开始标签，结束标签，自封闭标签）
+function data(c) {
+    if (c == '<') {
+        return tagOpen;
+    } else if (c == EOF) {
+        emit({
+            tagType: "EOF"
+        });
+        return;
+    } else {
+        emit({
+            tagType: "text",
+            content: c
+        });
+        return data; //除了左尖括弧外的字符都可以理解为文本节点
+    }
+}
+
+//<div
+function tagOpen(c) {
+    if (c == '/') {
+        return endTagOpen;
+    } else if (c.match(/^[a-zA-Z]$/)) {
+        currentToken = {
+            tagType: "startTag", //数据结构上，不管是否自封闭，统一认为startTag类型，用一个额外的变量isSelefClosing来标识是否自封闭
+            tagName: ""
+        }
+        return tagName(c);
+    } else if (c == '!') { //处理注释及DOCTYPE
+        return specialTagOpen
+    } else {
+        return;
+    }
+}
+
+function specialTagOpen(c) {
+    if (c == '-') {
+        return notesOpen1;
+    } else if (c == 'D') {
+        return docTypeTagOpen1;
+    }
+}
+
+function notesOpen1(c) {
+    if (c == '-') {
+        return notesOpen2;
+    } else {
+        //报错
+    }
+}
+
+function notesOpen2(c) {
+    if (c == '-') {
+        return notesClose1;
+    } else {
+        return notesOpen2;
+    }
+}
+
+function notesClose1(c) {
+    if (c == '-') {
+        return notesClose2;
+    } else {
+        //报错
+    }
+}
+
+function notesClose2(c) {
+    if (c == '>') {
+        return data;
+    } else {
+        //报错
+    }
+}
+
+function docTypeTagOpen1(c) {
+    if (c == 'O') {
+        return docTypeTagOpen2;
+    } else {
+        //报错
+    }
+}
+
+function docTypeTagOpen2(c) {
+    if (c == 'C') {
+        return docTypeTagOpen3;
+    } else {
+        //报错
+    }
+}
+
+function docTypeTagOpen3(c) {
+    if (c == 'T') {
+        return docTypeTagOpen4;
+    } else {
+        //报错
+    }
+}
+
+function docTypeTagOpen4(c) {
+    if (c == 'Y') {
+        return docTypeTagOpen5;
+    } else {
+        //报错
+    }
+}
+
+function docTypeTagOpen5(c) {
+    if (c == 'P') {
+        return docTypeTagOpen6;
+    } else {
+        //报错
+    }
+}
+
+function docTypeTagOpen6(c) {
+    if (c == 'E') {
+        return docTypeTagOpen7;
+    } else {
+        //报错
+    }
+}
+
+function docTypeTagOpen7(c) {
+    if (c == '>') {
+        return data;
+    } else {
+        return docTypeTagOpen7
+    }
+}
+
+function endTagOpen(c) {
+    if (c.match(/^[a-zA-Z]$/)) {
+        currentToken = {
+            tagType: "endTag",
+            tagName: ""
+        }
+        return tagName(c);
+    } else if (c == ">") {
+        //报错
+    } else if (c == EOF) {
+        //报错
+    } else {
+
+    }
+}
+
+//<html prop
+function tagName(c) {
+    if (c.match(/^[\t\n\f ]$/)) { //遇到tab符、换行符、禁止符（prohibited）、空格，说明tagName结束
+        return beforeAttributeName;
+    } else if (c == "/") {
+        return selfClosingStartTag;
+    } else if (c.match(/^[a-zA-Z]$/)) {
+        currentToken.tagName += c; //c.toLowerCase();
+        return tagName;
+    } else if (c == ">") { //说明这是一个普通的开始标签，结束掉这个标签w
+        emit(currentToken);
+        return data;
+    } else {
+        return tagName;
+    }
+}
+
+//<html attribute
+function beforeAttributeName(c) {
+    //这里暂不处理属性
+    if (c.match(/^[\t\n\f ]$/)) {
+        return beforeAttributeName;
+    } else if (c == '/' || c == ">" || c == EOF) {
+        return afterAttributeName(c);
+    } else if (c == "=") {
+        //报错
+    } else {
+        currentAttribute = {
+            name: "",
+            value: ""
+        }
+        //console.log("currentAttribute", currentAttribute);
+        return attributeName(c);
+    }
+}
+
+//<div class="abc" ></div> <div class="abc" />
+function attributeName(c) {
+    //console.log(currentAttribute);
+    if (c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
+        return afterAttributeName(c);
+    } else if (c == "=") {
+        return beforeAttributeValue;
+    } else if (c == "\u0000") { //空格
+
+    } else if (c == "\"" || c == "'" || c == "<") {
+
+    } else {
+        currentAttribute.name += c;
+        return attributeName;
+    }
+}
+
+function beforeAttributeValue(c) {
+    if (c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
+        return beforeAttributeValue;
+    } else if (c == "\"") {
+        return doubleQuotedAttributeValue;
+    } else if (c == "\'") {
+        return singleQuotedAttributeValue;
+    } else if (c == ">") {
+        //return data
+    } else {
+        return UnquotedAttributeValue(c);
+    }
+}
+
+function doubleQuotedAttributeValue(c) {
+    if (c == "\"") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    } else if (c == "\u0000") { //空格
+
+    } else if (c == EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return doubleQuotedAttributeValue;
+    }
+}
+
+function singleQuotedAttributeValue(c) {
+    if (c == "\'") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    } else if (c == "\u0000") { //空格
+
+    } else if (c == EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return singleQuotedAttributeValue;
+    }
+}
+
+function UnquotedAttributeValue(c) {
+
+}
+
+function afterQuotedAttributeValue(c) {
+    if (c.match(/^[\t\n\f ]$/)) {
+        return beforeAttributeName;
+    } else if (c == "/") {
+        return selfClosingStartTag;
+    } else if (c == ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (c == EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return afterQuotedAttributeValue;
+    }
+}
+
+function UnquotedAttributeValue() {
+    if (c.match(/^[\t\n\f ]$/)) {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return beforeAttributeName;
+    } else if (c == "/") {
+        currentToken[currentAttribute.name] = currentAttribute.vlue;
+        return selfClosingStartTag;
+    } else if (c == ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (c == "\u0000") {
+
+    } else if (c == "\"" || c == "'" || c == "<" || c == "=" || c == "`") {
+
+    } else if (c == EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return UnquotedAttributeValue;
+    }
+}
+
+function selfClosingStartTag(c) {
+    if (c == '>') {
+        currentToken.isSelfClosing = true;
+        emit(currentToken);
+        return data;
+    } else if (c == "EOF") {
+        //报错
+    } else {
+        //报错
+    }
+}
+
+function afterAttributeName(c) {
+    if (c.match(/^[\t\n\f ]$/)) {
+        return afterAttributeName;
+    } else if (c == "/") {
+        return selfClosingStartTag;
+    } else if (c == "=") {
+        return beforeAttributeValue;
+    } else if (c == ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (c == EOF) {
+
+    } else {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        currentAttribute = {
+            name: "",
+            value: ""
+        };
+        return attributeName(c);
+    }
+}
+
+
+module.exports.parseHTML = function parseHTML(html) {
+    //因为HTML标准里把初始状态叫做data
+    let state = data;
+    for (let c of html) {
+        state = state(c);
+    }
+    state = state(EOF); //作为最后的输入传给状态机
+    console.log(stack[0])
+}
+
+//第一课总结
+//为了方便文件管理，我们把parser单独拆到文件中
+//parser接受HTML文本作为参数，返回一棵DOM树
+
+//第二课总结
+//我们用FSM（有限状态机）来实现HTML的分析
+//在HTML标准中，已经规定了HTML的状态
+//Toy-Browser只挑选其中一部分状态，完成一个最简版本
+
+//第三课总结
+//主要的标签有：开始标签，结束标签和自封闭标签
+//在这一步我们暂时忽略属性
+
+//第四课总结
+//在状态机中，除了状态迁移，我们还会加入业务逻辑
+//我们在标签结束状态提交标签token
+
+//第五课总结
+//属性值分为单引号、双引号、无引号三种写法，因此需要较多状态处理
+//处理属性的方式跟标签类似
+//属性结束时，我们把属性加到标签Token上
+
+//第六课总结
+//从标签构建DOM树的基本技巧是使用栈
+//遇到开始标签时创建元素并入栈，遇到结束标签时出栈
+//自封闭节点可视为入栈后立刻出栈
+//任何元素是父元素是它入栈前的栈顶
