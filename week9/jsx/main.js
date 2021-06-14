85964395a18fc66436d5635bc68b4f0732afb2cd
yeahzeyu <819899147@qq.com>
2021-06-14 19:53:12 +0800

    week9

--- week9/jsx/main.js
+++ week9/jsx/main.js
@@ -1,5 +1,6 @@
 import { Component, createElement } from "./framework.js"
 import { Carousel } from "./carousel.js"
+import { Timeline, Animation } from "./animation.js"
 
 
 let d = [
@@ -16,3 +17,10 @@ a.mountTo(document.body);
 //在最新版的DOM标准里面是有办法的，需要注册一下自定义标签/元素的名称和类型
 //但是现行的比较安全的浏览器版本里面，不建议这样做
 //所以在使用element的时候，建议使用一个另外的接口，这里用了反向的操作
+
+let tl = new Timeline();
+window.tl = tl;
+window.animation = new Animation({set a(v) { console.log(v)}}, "a", 0, 100, 1000, null);
+//不需要频繁地开启或重置timeLine了，可以支持动态地向一个timeLine去添加animation
+//tl.add(new Animation({}, "a", 0, 100, 1000, null));
+tl.start();
